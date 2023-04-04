a03067412e857f21301249ace80d3f742301ee84
Raphael <pusam.raphy@gmail.com>
2023-04-04 16:36:28 +0200

    header component + navbar component

--- Pages/index.php
+++ Pages/index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
-    <?php require ('./Components/head.html'); ?>
-    
+    <?php require ('./Components/head.html'); ?> 
     <body>
+        <?php require ('./Components/navbar.html'); ?>
+
         <h1> Test </h1>
 
     </body>
