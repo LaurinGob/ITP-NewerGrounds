623dbc0fa853236fbe5a6de2823c8cb2007b4ee2
Ceraplex <lino-breuss@gmx.at>
2023-04-17 11:20:05 +0200

    PC

--- Pages/index.php
+++ Pages/index.php
@@ -3,8 +3,25 @@
     <?php require ('./Components/head.html'); ?> 
     <body>
         <?php require ('./Components/navbar.html'); ?>
+        <div class="row">
+            <div class="container col-3">
+<!--                Sidebar-->
+                <?php require ('./Components/sidebar.html')?>
+            </div>
+            <div class="container col-9">
+<!--                Site-content-->
+                <h1> Test </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+<!--                Footer-->
+                <?php require ('./Components/footer.html')?>
+            </div>
+        </div>
 
-        <h1> Test </h1>
+
+
 
     </body>
 </html>
