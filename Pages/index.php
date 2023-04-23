4face19bee2a17e5a7aec64ef1fef8b0438c1561
Raphael <pusam.raphy@gmail.com>
2023-04-23 13:35:02 +0200

    rock_thumbnail and game boxes

--- Pages/index.php
+++ Pages/index.php
@@ -5,23 +5,38 @@
         <?php require ('./Components/navbar.html'); ?>
         <div class="row">
             <div class="container col-3">
-<!--                Sidebar-->
+                <!--Sidebar-->
                 <?php require ('./Components/sidebar.html')?>
             </div>
-            <div class="container col-9">
-<!--                Site-content-->
-                <h1> Test </h1>
+            <div class="container col-9 text-center">
+                <!--Site-content-->
+                <h1> Finest Selection </h1>
+                <div id="gameDisplayBox" class="w-100">
+                    <div class="gameCollumn w-100 d-flex mt-4">
+                        <div class="container w-25 bg-secondary"> <img src="../Resources/rock_thumbnail.png" class="thumbnail" alt="Game 1"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 2"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 3"/> </div>
+                    </div>
+
+                    <div class="gameCollumn w-100 d-flex mt-4">
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 4"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 5"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 6"/> </div>
+                    </div>
+
+                    <div class="gameCollumn w-100 d-flex mt-4">
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 7"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 8"/> </div>
+                        <div class="container w-25 bg-secondary"> <img src="#" alt="Game 9"/> </div>
+                    </div>
+                <div>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-<!--                Footer-->
+                <!--Footer-->
                 <?php require ('./Components/footer.html')?>
             </div>
         </div>
-
-
-
-
     </body>
 </html>
