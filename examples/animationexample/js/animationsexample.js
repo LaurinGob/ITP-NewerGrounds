a549b481c7cdc3062a63e572453f16dc84aac87a
Laurin <laurin.gobber@outlook.com>
2023-04-25 16:25:08 +0200

    AnimationExample

--- examples/animationexample/js/animationsexample.js
+++ examples/animationexample/js/animationsexample.js
@@ -1,7 +1,7 @@
 // #################### SETUP
 // constants
 const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 400;
+const SCREEN_HEIGHT = 800;
 const BG_COLOR = 0xCCCCFF;
 const MAX_FPS = 60;
 
@@ -27,17 +27,29 @@ for (let i = 1; i <= 8; i++) {
     textureArray.push(PIXI.Texture.from("res/textures/running"+i+".png"));
 }
 
-let runningGuy = new PIXI.AnimatedSprite(textureArray);
+let runningGuy1 = new PIXI.AnimatedSprite(textureArray);
 
-// #################### setting up sprite
-runningGuy.anchor.set(0.5);
-runningGuy.position.x = SCREEN_WIDTH/2;
-runningGuy.position.y = SCREEN_HEIGHT/2;
-runningGuy.animationSpeed = 0.2;
-GAME.stage.addChild(runningGuy);
+// #################### setting up sprite runningGuy1
+runningGuy1.anchor.set(0.5);
+runningGuy1.position.x = SCREEN_WIDTH/2;
+runningGuy1.position.y = SCREEN_HEIGHT/2 - 200;
+runningGuy1.animationSpeed = 0.2;
+GAME.stage.addChild(runningGuy1);
+
+let runningGuy2 = new PIXI.AnimatedSprite(textureArray);
+
+// #################### setting up sprite runningGuy2
+runningGuy2.anchor.set(0.5);
+runningGuy2.position.x = SCREEN_WIDTH/2;
+runningGuy2.position.y = SCREEN_HEIGHT/2 + 200;
+runningGuy2.animationSpeed = 0.1;
+GAME.stage.addChild(runningGuy2);
 
 function gameLoop(delta) {
-    runningGuy.play();
+    runningGuy1.play(); // plays animation in a loop
+    runningGuy2.play(); // plays half speed > change with animation speed (animation speed 1 == every frame is changing animation)
+    //runningGuy2.gotoAndStop(3) // goes to frame number 4 > use to animate
+    //runningGuy2.gotoAndPlay(3) // plays from frame 4 > use to animate from a certain point onward e.i. go into attack and play attack
 }
 
 // http://localhost/ITP-NewerGrounds/examples/animationexample/animationsexample.html
