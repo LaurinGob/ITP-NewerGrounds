664f23a9824b27f13eb0e02512afe095133c67fa
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2023-04-27 17:47:19 +0200

    player example

--- examples/playerexample/js/playerexample.js
+++ examples/playerexample/js/playerexample.js
@@ -16,31 +16,51 @@ const GAME = new PIXI.Application(
 
 // adds pixi canvas to selected dom
 document.getElementById("canvasAnchor").appendChild(GAME.view);
-
 // adds the gameloop function to ticker (pixi.js)
+
+// #################### scene setup (loading of all objects)
+var SHEET;
+var player;
+var gameLoaded;
+loadRessources()
+
 GAME.ticker.add(gameLoop);
 GAME.ticker.maxFPS = MAX_FPS;
 
-// #################### scene setup (loading of all objects)
-// loader.add("spritesheet", "res/adventurer_spritesheet.png");
-
-const sheet = PIXI.Assets.load('res/adventurer_spritesheet.json');
-const spritesheet = new PIXI.Spritesheet(PIXI.BaseTexture.from(sheet.meta.image), sheet)
-
-
-// loader.load((loader, resources) => {
-//     sprites.spritesheet = new PIXI.TilingSprite(resources.spritesheet.texture);
-// });
-
-let player = new PIXI.AnimatedSprite(spritesheet.animations.idle);
-player.anchor.set(0.5);
-player.position.x = SCREEN_WIDTH/2;
-player.position.y = SCREEN_HEIGHT/2;
-GAME.stage.addChild(player);
+player_running = false
 
 // #################### gameloop
 function gameLoop(delta) {
-    player.play();
+    if (gameLoaded) {
+        if (keys['68']) {
+            if (!player_running) {
+                player.textures = SHEET.animations['adventurer-run'];
+                player_running = true;
+                player.play();
+            }
+        } else if (!keys['68']) { // TODO: WARUMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+            player.textures = SHEET.animations['adventurer-idle'];
+            player.play();
+            player_running = false;
+        }
+    }
+}
+
+async function loadRessources() {
+    SHEET = await PIXI.Assets.load('res/player/player.json');
+    loadstuff();
+}
+async function loadstuff() {
+    player = new PIXI.AnimatedSprite(SHEET.animations['adventurer-idle']);
+    player.texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    player.anchor.set(0.5);
+    player.position.x = SCREEN_WIDTH/2;
+    player.position.y = SCREEN_HEIGHT/2;
+    player.animationSpeed = 0.1;
+    player.scale.set(5);
+    player.play()
+    GAME.stage.addChild(player);
+    gameLoaded = true;
 }
 
 // http://localhost/ITP-NewerGrounds/examples/playerexample/playerexample.html
