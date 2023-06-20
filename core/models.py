cffbfddb1e5503aa184c04666bfb536d7859dd5b
Emile Durkheim <github@andretsch.at>
2023-06-20 23:20:37 +0200

    minor cleanup

--- core/models.py
+++ core/models.py
@@ -6,10 +6,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, username, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
+        """ Creates and saves a User with the given email and password. """
         if not email:
             raise ValueError('Users must have an email address')
 
@@ -23,10 +20,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, username, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
+        """ Creates and saves a superuser with the given email and password. """
         user = self.create_user(
             email,
             password=password,
@@ -49,21 +43,3 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
-
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
-
-    
